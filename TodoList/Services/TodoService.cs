89fa68d5df670945257837d9d2d13fbe8a46e70d
Vinay-Reddy-Thadakala <vinayreddythadakala@gmail.com>
2025-04-12 18:51:24 +0530

    Initial commit: Created Blazor Server To-Do List app

---
+++ TodoList/Services/TodoService.cs
@@ -0,0 +1,42 @@
+﻿
+namespace TodoList.Services
+{
+    public class TodoService : ITodoService
+    {
+        private readonly IList<TodoItem> _todoItems;
+
+        public TodoService()
+        {
+            _todoItems = new List<TodoItem>
+            {
+                new TodoItem("Wash Clothes"),
+                new TodoItem("Clean Desk")
+            };
+        }
+
+        void ITodoService.Add(TodoItem item)
+        {
+            _todoItems.Add(item);
+        }
+
+        void ITodoService.Delete(TodoItem item)
+        {
+            _todoItems.Remove(item);
+        }
+
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return _todoItems.ToList();
+        }
+
+        public void Complete(TodoItem item)
+        {
+            item.Completed = true;
+        }
+
+        public void Uncomplete(TodoItem item)
+        {
+            item.Completed = false;
+        }
+    }
+}   
